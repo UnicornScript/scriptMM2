1f8433c67f1ccccf298c9f939870cd808ddddc75
UnicornScript <lajv1986@gmail.com>
2026-07-06 18:36:17 +0500

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnicornScript/scriptMM2/refs/heads/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnicornScript/scriptMM2/refs/heads/main/spawner.lua"))()
