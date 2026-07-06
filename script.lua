140fb36161be0ade3ef2394d62a673241ab8d8ed
UnicornScript <lajv1986@gmail.com>
2026-07-06 18:30:43 +0500

    Delete script.lua

--- script.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UnicornScript/scriptMM2/refs/heads/main/spawner.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UnicornScript/scriptMM2/refs/heads/main/s.lua"))()
